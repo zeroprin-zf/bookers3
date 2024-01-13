cc08091795a7c070142b3fe9e164a003c6b9854b
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-01-13 02:16:41 +0000

    bootstrap導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
+//jQueryはJavaScriptの記述を簡素化し、互換性や便利な機能を提供する者
+//jQueryはライブラリ言語
