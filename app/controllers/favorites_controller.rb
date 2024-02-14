64ccc807266a6fe7898e22b975b9ff93172be175
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-02-14 13:12:33 +0000

    テンプレート作成まで

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,6 +7,10 @@ class FavoritesController < ApplicationController
  end
 
  def destroy
+  book = Book.find(params[:book_id])
+  favorite = current_user.favorites.find_by(book_id: book.id)
+  favorite.destroy
+  redirect_to request.referer
  end
 
 end
