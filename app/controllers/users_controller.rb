ba40b38304db862a7c21def83a42612c1dd83a00
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-03-02 16:21:27 +0000

    roomコントローラ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :authenticate_user!, only: [:show]
+  
   def index
     @users = User.all
     @user = current_user
@@ -7,6 +10,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
     @books = @user.books
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update] do
+    resources :messages, only: [:create]
+    resources :rooms, only: [:create, :show]
     resource :relationships, only: [:create, :destroy]
      get "followings" => "relationships#followings", as: "followings"
      get "followers" => "relationships#followers", as: "followers"
