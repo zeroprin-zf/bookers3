22a3c7f0440c435432fd95f73d75c4668bd9e27d
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-03-02 11:11:44 +0000

    コントローラ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 
   def follow(user)
     active_relationships.create(followed_id: user.id)
