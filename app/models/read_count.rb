a2f2a4b735e9fa00f83d1932bec98bb63f769f9c
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-03-07 14:20:31 +0000

    閲覧数作成

---
+++ app/models/read_count.rb
@@ -0,0 +1,2 @@
+class ReadCount < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_02_104623) do
+ActiveRecord::Schema.define(version: 2024_03_07_141847) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,13 @@ ActiveRecord::Schema.define(version: 2024_03_02_104623) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "read_counts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "relationships", force: :cascade do |t|
