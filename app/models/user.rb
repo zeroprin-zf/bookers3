8de77f366b000f75c294675ce02e947e1b6bb354
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-03-07 14:25:43 +0000

    アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :read_counts, dependent: :destroy
 
   def follow(user)
     active_relationships.create(followed_id: user.id)
