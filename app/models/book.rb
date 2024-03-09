2a520bcad24498656182e346eaccfbcffacfb5e6
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-03-09 01:44:15 +0000

    投稿数のモデル

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,6 +9,11 @@ class Book < ApplicationRecord
   has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
   has_many :read_counts, dependent: :destroy
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+   scope :created_this_week, -> { where(created_at: 6.day.ago.beggining_of_day..Time.zone.now.end_of_day) }
+   scope :created_last_week, -> { where(created_at: 2.week.ago.begginning_of_day..1.week.ago.end_of_day)         }
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
